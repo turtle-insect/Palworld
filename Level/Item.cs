c49e331a3b76ea38f5b476f720d46a4ff6574430
turtle-insect <turtle.insect@gmail.com>
2024-01-31 00:43:11 +0900

    support Item's name

--- Level/Item.cs
+++ Level/Item.cs
@@ -8,12 +8,24 @@ namespace Level
 {
 	internal class Item
 	{
+		private readonly uint mNameAddress;
 		private readonly uint mCountAddress;
 
 		public Item(uint address)
 		{
-			var address_list = SaveData.Instance().FindAddress("StackCount", address);
+			var address_list = SaveData.Instance().FindAddress("NameProperty", address);
+			if (address_list.Count > 0) mNameAddress = address_list[0] + 22;
+			address_list = SaveData.Instance().FindAddress("StackCount", address);
 			if (address_list.Count > 0) mCountAddress = address_list[0] + 36;
+		}
+
+		public String Name
+		{
+			get
+			{
+				var length = (uint)SaveData.Instance().ReadNumber(mNameAddress, 4);
+				return SaveData.Instance().ReadText(mNameAddress + 4, length);
+			}
 		}
 
 		public UInt64 Count
